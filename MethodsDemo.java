9121191fd81d636740456dca291d0e73db5d7f06
Djoyke Reijans <crudendjoyke@gmail.com>
2023-12-28 20:10:20 +0100

    Method lesson

---
+++ MethodsDemo.java
@@ -0,0 +1,44 @@
+public class MethodsDemo
+{
+    public static void main(String[] args)
+    {
+        printHello();
+        printNumber(10);
+        int result = giveMe10();
+        System.out.println(result);
+
+        result = addThese(3, 5);
+        System.out.println(result);
+        
+        System.out.println(squared(5.5));
+    }//end main
+
+    //void, parameterless method
+    public static void printHello()
+    {
+        System.out.println("Hello there");
+    }//end printHello
+
+    //void, parametrized method
+    public static void printNumber(int a)
+    {
+        System.out.println("The number is " + a);
+    }// end printNumber
+
+    //value-returning, parameterless method
+    public static int giveMe10()
+    {
+        return (10);
+    }//end giveMe10
+
+    //value-returning, parameterized method
+    public static int addThese(int num1, int num2)
+    {
+        return num1 + num2;
+    }// end addThese
+
+    public static double squared(double a)
+    {
+        return (a *= a);
+    }//end squared
+}
