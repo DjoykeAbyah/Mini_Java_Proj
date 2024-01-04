8a8059883bd035b081269ec62412fdc4f64205a2
DjoykeReijans <crudendjoyke@gmail.com>
2024-01-04 17:00:08 +0100

    start tictactoe

---
+++ TicTacToe.java
@@ -0,0 +1,44 @@
+public class TicTacToe
+{
+    public static void main(String[] args)
+    {
+
+
+    }    //end main
+
+    public static void runGame()
+    {
+
+    }//end runGame
+
+    public static void initGameBoard(String[][] gameBoard)
+    {
+
+    }// end initGameBoard
+
+    public static void printBoard(String[][] gameBoard)
+    {
+
+    }// end printBoard
+
+    public static void getUserInput(boolean xTurn, String[][] gameBoard)
+    {
+
+    }// end getUserInput
+
+    public static void cellOccupied(int row, int col, String[][] gameBoard)
+    {
+        //return false?
+    }// end cellOccupied
+
+    public static String getWinner(String[][] gameBoard)
+    {
+        return "";
+    }// end getWinner
+
+    public static boolean isBoardFull(String[][] gameBoard)
+    {
+        return false;
+    }// end isBoardFull
+    
+}// end class
