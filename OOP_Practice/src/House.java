a7341253ca3636939276d5a2881272b223102139
DjoykeReijans <crudendjoyke@gmail.com>
2024-01-05 12:58:08 +0100

    refactoring done on HouseDemo

--- OOP_Practice/src/House.java
+++ OOP_Practice/src/House.java
@@ -1,21 +1,47 @@
 public class House
 {
-    //instance fields
-    //fields live as long as object lives
+    /*
+        instance fields
+        fields live as long as object lives
+     */
     private int numStories;
     private int numWindows;
     private String color;
 
-    /**
-     *  methods don't have static, because these are instance methods
-     *  they're called on an object
-     *  they refer to a specific object not the entire class
+    /*
+        constructor have the same name as the class
+        if you don't write one you get one without args is default constructor.
+    */
+
+    //no arg constructor
+    public House()
+    {
+        numStories = 1;
+        numWindows = 4;
+        color = "grey";
+    }// end ctor
+
+    //parameterized constructor
+    public House (int numStories, int numWindows, String color)
+    {
+        this.numStories = numStories;
+        this.numWindows = numWindows;
+        this.color = color;
+    }// end param ctor
+
+    /*
+        methods don't have static, because these are instance methods
+        they're called on an object
+        they refer to a specific object not the entire class
      */
+
+    //accessors
     public int getNumStories()
     {
         return numStories;
     }
 
+    //mutators
     public void setNumStories(int numStories)
     {
         //this keyword overcomes name shadowing
@@ -41,8 +67,5 @@ public class House
     {
         this.color = color;
     }
-
-
-
 
 }//End House
--- OOP_Practice/src/HouseDemo.java
+++ OOP_Practice/src/HouseDemo.java
@@ -2,23 +2,29 @@ public class HouseDemo
 {
     public static void main(String[] args)
     {
-        House myHouse = new House();
-        House yourHouse = new House();
+        /*
+            they each get their own copy of those variables
+            when we call those methods
+            the instance methods are only applied to the specific house
+            because it's an instance method
+            the data it's returning are instance fields
+            each object gets its own copy
 
-        myHouse.setNumStories(2);
-        myHouse.setNumWindows(6);
-        myHouse.setColor("red");
+            these objects maintain their own specific data
+            we can treat the objects as self-contained entities
+            pass them to methods etc.
+         */
+        House myHouse = new House(2, 6, "red");
+        House yourHouse = new House(3, 10, "blue");
 
-        yourHouse.setNumStories(3);
-        yourHouse.setNumWindows(20);
-        yourHouse.setColor("blue");
-
-        System.out.println("Your house is " + yourHouse.getColor() +
-                " has " + yourHouse.getNumStories() + " stories and " +
-                yourHouse.getNumWindows() + " windows.");
-        System.out.println();
-        System.out.println("My house is " + myHouse.getColor() +
-                " has " + myHouse.getNumStories() + " stories and " +
-                myHouse.getNumWindows() + " windows.");
+        PrintHouseData(myHouse);
+        PrintHouseData(yourHouse);
     }//end main
+
+    public static void PrintHouseData(House Ahouse)
+    {
+        System.out.println("Your house is " + Ahouse.getColor() +
+                " has " + Ahouse.getNumStories() + " stories and " +
+                Ahouse.getNumWindows() + " windows.");
+    }//end printHouseData
 }//end HouseDemo
