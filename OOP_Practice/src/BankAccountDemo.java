bdaf5d891eeff8034b0975a2f4117972b91898dc
DjoykeReijans <crudendjoyke@gmail.com>
2024-01-11 20:54:42 +0100

    added account

--- OOP_Practice/src/BankAccountDemo.java
+++ OOP_Practice/src/BankAccountDemo.java
@@ -3,6 +3,7 @@ public class BankAccountDemo
     public static void main(String[] args)
     {
         BankAccount Djoyke = new BankAccount("Djoyke");
+        BankAccount Jeff = new BankAccount("Jeff", 1000);
 
         PrintOwner(Djoyke);
         Djoyke.deposit(0);
@@ -10,6 +11,13 @@ public class BankAccountDemo
         Djoyke.withdraw(600);
         Djoyke.withdraw(40);
         Djoyke.withdraw(-5);
+
+        PrintOwner(Jeff);
+        Jeff.deposit(0);
+        Jeff.deposit(500);
+        Jeff.withdraw(600);
+        Jeff.withdraw(40);
+        Jeff.withdraw(-5);
     }
 
     public static void PrintOwner(BankAccount bankAccount)
